008e1a1ebc2d79cce90854010d086d246315f137
Rahma Maulida <rahma.maulida68@gmail.com>
2022-06-29 21:51:54 +0700

    transaction history

--- models/transaction.js
+++ models/transaction.js
@@ -9,10 +9,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Transaction.belongsTo(models.User, {
-        foreignKey: "user_id",
-        as: "users",
-      });
+      // Transaction.belongsTo(models.User, {
+      //   foreignKey: "user_id",
+      //   as: "users",
+      // });
       Transaction.belongsTo(models.Product, {
         foreignKey: "product_id",
         as: "products",
